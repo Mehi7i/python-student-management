3678148fc92fcdb20687fc343784f5b955498a71
m4hi7i <mahdisouri62@gmail.com>
2026-07-20 20:12:36 +0330

    Improve input validation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def save_students():
     print(os.getcwd())
     with open("students.json", "w") as file:
         json.dump(students,file)
-
+# --------------------------------------------------
 def load_students():
     global students
     try:
@@ -13,21 +13,35 @@ def load_students():
             students=json.load(file)
     except FileNotFoundError:
         students=[]
-
+# ----------------------------------------------------
 def add_student():
-
-    name=input("enter  name:")
-    age=int(input("enter  age:"))
-    score=float(input("enter  score:"))
-    student={
-    "name":name,
-    "age":age,
-    "score":score
-     }
-    students.append(student)
-    save_students()
-    print("Student Added Successfully")
-
+    try:
+       
+        name=input("enter  name:")
+        while True:
+            try:
+                age=int(input("enter  age:"))
+                break
+            except ValueError:
+                  print("incorrect input")
+        while True:          
+            try:
+                score=float(input("enter  score:"))
+                break
+            except ValueError:
+                print("incorrect input")
+        student={
+        "id":get_next_id(),
+        "name":name,
+        "age":age,
+        "score":score
+        }
+        students.append(student)
+        save_students()
+        print("Student Added Successfully")
+    except ValueError:
+         print("incorrect input")
+         return
 # ----------------------------------------
 def show_student():
      if not students:
@@ -35,6 +49,7 @@ def show_student():
            return
      else:
         for student in students:
+            print(f"Id : {student['id']}")
             print(f"Name : {student['name']}")
             print(f"Age  : {student['age']}")
             print(f"Score: {student['score']}")
@@ -42,58 +57,105 @@ def show_student():
 
 # ---------------------------------------
 def search_student():
-    vorudi=input(f"what name:")
-    found=False
-    for student in students:
-        if vorudi==student["name"]:
-            found=True
-            print(f"Name: {student['name']}, Age: {student['age']}, Score: {student['score']}")
-            break
-    if not found:
-        print("not found")
+    try:
+        vorudi = int(input("what ID:"))
+
+        found = False
+
+        for student in students:
+            if vorudi == student["id"]:
+                found = True
+                print(
+                    f"Name: {student['name']}, "
+                    f"Age: {student['age']}, "
+                    f"Score: {student['score']}, "
+                    f"ID: {student['id']}"
+                )
+                break
+
+        if not found:
+            print("not found")
+
+    except ValueError:
+        print("incorrect input")      
 # ----------------------------------------
 def edit_student():
-    vorudi=input(f"what name:")
-    found=False
-    for student in students:
-        if vorudi==student["name"]:
-            found=True
-            vorudy=int(input(f"whch for change: 1-edit age:{student['age']}, 2-edit score:{student['score']}:, 3-edit name:{student['name']}"))
-            if vorudy==1:
-                new_age=int(input("new age?"))
-                student["age"]=new_age
-                print("saved")
-                save_students()
+    try:
+        while True:
+            try:
+                vorudi=int(input(f"what id:"))
                 break
-            elif vorudy==2:
-                new_score=float(input("new score?"))
-                student["score"]=new_score
-                save_students()
-                print("saved")
-                break
-            elif vorudy==3:
-                new_name=input("new name?")
-                student["name"]=new_name
-                print("saved")
-                save_students()    
-            else:
-                print("wrong input")
-    if not found:
-        print("not dound your student")
-
+            except ValueError:
+                print("incorrect input") 
+        found=False
+        for student in students:
+            if vorudi==student["id"]:
+                found=True
+                while True:
+                    try:
+                        vorudy=int(input(f"whch for change: 1-edit age:{student['age']}, 2-edit score:{student['score']}:, 3-edit name:{student['name']}"))
+                        if vorudy in [1, 2, 3]:
+                            break
+                        else:
+                            print("wrong input")
+                    except ValueError:
+                        print("incorrect input")
+                if vorudy==1:
+                    while True:
+                        try:
+                            new_age=int(input("new age?"))
+                            student["age"]=new_age
+                            print("saved")
+                            break
+                        except ValueError:
+                            print("incorrect input")
+                    save_students()
+                    break
+                elif vorudy==2:
+                    while True:
+                        try:
+                            new_score=float(input("new score?"))
+                            student["score"]=new_score
+                            save_students()    
+                            print("saved")
+                            break
+                        except ValueError:
+                            print("incorrect input")
+                elif vorudy==3:
+                    new_name=input("new name?")
+                    student["name"]=new_name
+                    print("saved")
+                    save_students()
+                    break                     
+        if not found:
+            print("not dound your student")
+    except ValueError:
+        print("incorrect input")
 # -------------------------------------------
 def del_student():
-    entekhab=input("which student?")
-    found=False
-    for student in students:
-        if entekhab==student["name"]:
-            found= True
-            students.remove(student)
-            save_students()
-            print("saved")
-            break
-    if not found:
-        print("not found")
+    try:
+        student_id=int(input("which student?"))
+        found=False
+        for student in students:
+            if student_id==student["id"]:
+                found= True
+                students.remove(student)
+                save_students()
+                print("saved")
+                break
+        if not found:
+            print("not found")
+    except ValueError:
+            print("incorrect input")
+# ------------------------------------------------------
+def get_next_id():
+    if not students:
+        return 1
+    else:
+        return students[-1]["id"] + 1
+      
+
+
 
 
 # -------------------------------------------
@@ -104,7 +166,7 @@ while True:
     print("3. Search Student")
     print("4. Edit Student")
     print("5. Delete Student")
-    print("6. Exiف")
+    print("6. Exit")
     choice=input("select:")
     print("\n")
 
